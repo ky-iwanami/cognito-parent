13ffa09507535e167463471fe10a8185466a4d2c
TRUST-1909N074\ky_iwanami <ky_iwanami@trusttokyo.onmicrosoft.com>
2025-06-29 09:34:59 +0900

    プロジェクト名を「amplify-vite-react-template」から「cognito-parent」に変更し、依存関係のバージョンを更新しました。また、Cognitoのカスタムドメイン設定やOAuth設定を追加し、アプリケーションの初期化処理を実装しました。

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,57 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { userPool } = backend.auth.resources;
+
+// ドメインプレフィックス
+const domainPrefix = 'trust-parent';
+
+// カスタムドメインを使用
+userPool.addDomain('custom-domain', {
+  cognitoDomain: {
+    domainPrefix
+  }
+});
+
+// OAuth設定
+const { cfnUserPoolClient } = backend.auth.resources.cfnResources;
+
+// scope設定
+cfnUserPoolClient.allowedOAuthScopes = [
+  'openid',
+  'aws.cognito.signin.user.admin'
+];
+
+// 環境変数に設定された子アプリのドメインを取得
+const TARGET_DOMAIN = process.env.TARGET_DOMAIN?.split(',') || [];
+
+// redirectSignIn設定
+cfnUserPoolClient.callbackUrLs = [
+  'http://localhost:5173/',
+  ...TARGET_DOMAIN,
+];
+
+// redirectSignOut設定
+cfnUserPoolClient.logoutUrLs = [
+  'http://localhost:5173/',
+  ...TARGET_DOMAIN,
+];
+
+// responseType設定（認可コードフロー）
+cfnUserPoolClient.allowedOAuthFlows = ['code'];
+cfnUserPoolClient.allowedOAuthFlowsUserPoolClient = true;
+
+// ドメインだけamplify_output.jsonのauthブロックに出力されないため、カスタムで出力する
+const domain = `${domainPrefix}.auth.ap-northeast-1.amazoncognito.com`;
+backend.addOutput({
+  custom: {
+    cognitoDomain: domain,
+  } 
+});
+
+export default backend;
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+import { AuthUser, getCurrentUser, signInWithRedirect, signOut } from "aws-amplify/auth";
 
 const client = generateClient<Schema>();
 
+const origin = `${window.location.origin}/`;
+outputs.auth.oauth = {
+  ...outputs.auth.oauth,
+  domain: outputs.custom.cognitoDomain,
+  redirect_sign_in_uri: [origin],
+  redirect_sign_out_uri: [origin],
+}
+Amplify.configure(outputs);
+
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [user, setUser] = useState<AuthUser | undefined>(undefined);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const user = await getCurrentUser();
+        setUser(user);
+      } catch (error) {
+        await signInWithRedirect({ options: { lang: "ja" } });
+      }
+    };
+    init();
   }, []);
 
   function createTodo() {
@@ -19,6 +44,11 @@ function App() {
 
   return (
     <main>
+      <div>
+        <p>親アプリです</p>
+        <p>User: {user?.username || 'No user'}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
